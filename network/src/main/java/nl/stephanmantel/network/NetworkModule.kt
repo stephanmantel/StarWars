2aaf5b91ff08cd5df15e058d234e4152b6938fb1
Stephan Mantel <stephan.mantel@gmail.com>
2019-01-02 14:39:07 +0100

    Create network module

---
+++ network/src/main/java/nl/stephanmantel/network/NetworkModule.kt
@@ -0,0 +1,32 @@
+package nl.stephanmantel.network
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import org.koin.dsl.module.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+val NetworkModule = module {
+
+    single<OkHttpClient> {
+        OkHttpClient.Builder()
+            .build()
+    }
+
+    single<Gson> {
+        GsonBuilder()
+            .create()
+    }
+
+    single<Retrofit> {
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.SERVER_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(get()))
+            .client(get())
+            .build()
+    }
+
+}
