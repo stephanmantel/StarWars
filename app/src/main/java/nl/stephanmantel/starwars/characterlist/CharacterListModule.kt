3f19c025f654df087cf035a53efc9a487293b8f9
Stephan Mantel <stephan.mantel@gmail.com>
2019-01-02 14:52:52 +0100

    Provide dependency with Koin

--- app/src/main/java/nl/stephanmantel/starwars/characterlist/CharacterListModule.kt
+++ app/src/main/java/nl/stephanmantel/starwars/characterlist/CharacterListModule.kt
@@ -6,7 +6,7 @@ import org.koin.dsl.module.module
 val CharacterListModule = module {
 
     single {
-        CharacterListRepository()
+        CharacterListRepository(get())
     }
 
     viewModel {
