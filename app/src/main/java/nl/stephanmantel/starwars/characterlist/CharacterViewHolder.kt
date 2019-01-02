f3f9991ae29f69e007d93988224fad78789041aa
Stephan Mantel <stephan.mantel@gmail.com>
2019-01-02 15:34:45 +0100

    Create a view holder for character items

---
+++ app/src/main/java/nl/stephanmantel/starwars/characterlist/CharacterViewHolder.kt
@@ -0,0 +1,28 @@
+package nl.stephanmantel.starwars.characterlist
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.list_item_character.view.*
+import nl.stephanmantel.domain.Character
+import nl.stephanmantel.starwars.R
+
+class CharacterViewHolder(
+    override val containerView: View
+): RecyclerView.ViewHolder(containerView), LayoutContainer {
+
+    companion object {
+        fun newInstance(parent: ViewGroup): CharacterViewHolder {
+            val context = parent.context
+            val itemView = LayoutInflater.from(context).inflate(R.layout.list_item_character, parent, false)
+            return CharacterViewHolder(itemView)
+        }
+    }
+
+    fun bind(character: Character) {
+        itemView.nameTextView.text = character.name
+    }
+
+}
