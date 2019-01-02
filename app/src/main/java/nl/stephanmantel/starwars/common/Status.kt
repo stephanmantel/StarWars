370b8d94f760533f492fe0482044e10cb013b423
Stephan Mantel <stephan.mantel@gmail.com>
2019-01-02 14:09:35 +0100

    Add Resource and Status classes
    These shared generics tie data to a state for views to display

---
+++ app/src/main/java/nl/stephanmantel/starwars/common/Status.kt
@@ -0,0 +1,11 @@
+package nl.stephanmantel.starwars.common
+
+/**
+ * Created by Stephan on 31/01/2018.
+ * The Status used by the Resource, communicated from view model to view
+ */
+enum class Status {
+    LOADING,
+    SUCCESS,
+    ERROR,
+}
