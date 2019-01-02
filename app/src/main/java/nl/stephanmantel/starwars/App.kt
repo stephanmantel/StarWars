7d5bcccdc36708ef6a7bc253e88b841cac343a68
Stephan Mantel <stephan.mantel@gmail.com>
2019-01-02 14:06:56 +0100

    Ad Koin for dependency injection

---
+++ app/src/main/java/nl/stephanmantel/starwars/App.kt
@@ -0,0 +1,15 @@
+package nl.stephanmantel.starwars
+
+import android.app.Application
+import nl.stephanmantel.starwars.characterlist.CharacterListModule
+import org.koin.android.ext.android.startKoin
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(this, listOf(
+            CharacterListModule
+        ))
+    }
+}
