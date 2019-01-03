b4d59291c5bfcffdb8730f840b4e21e8a7dbbfd7
Stephan Mantel <stephan.mantel@gmail.com>
2019-01-03 10:39:27 +0100

    Map birthdate from raw to refined character

--- network/src/main/java/nl/stephanmantel/network/rawdomain/character/CharacterMapper.kt
+++ network/src/main/java/nl/stephanmantel/network/rawdomain/character/CharacterMapper.kt
@@ -9,7 +9,8 @@ class CharacterMapper: Mapper<CharacterRaw, Character>() {
     override fun map(raw: CharacterRaw): Character {
         val name = raw.name ?: { throw EssentialParamsMissingException(listOf("name"), raw)}()
         return Character(
-            name
+            name,
+            mapBirthYear(raw.birthYear)
         )
     }
 
@@ -24,4 +25,20 @@ class CharacterMapper: Mapper<CharacterRaw, Character>() {
             throw EssentialParamsMissingException(missingParams, raw)
         }
     }
+
+    private fun mapBirthYear(raw: String?): Float? {
+        if (raw.isNullOrEmpty() || raw == "unknown") return null
+
+        val regex = Regex("([0-9]+\\.?)+([A-Z]+)")
+        val groups = regex.find(raw)?.groupValues
+        return groups?.let {
+            if (groups.size != 2) return null
+            val year = groups[0].toFloat()
+            val era = groups[1]
+
+            val eraMultiplier = if (era == "BBY") -1 else 1
+            year * eraMultiplier
+        }
+    }
+
 }
