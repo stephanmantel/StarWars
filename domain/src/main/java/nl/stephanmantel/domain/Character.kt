9da73b1e1f9285086110778b206b5d08fd3389cf
Stephan Mantel <stephan.mantel@gmail.com>
2019-01-03 14:06:08 +0100

    Make Character parcelable

--- domain/src/main/java/nl/stephanmantel/domain/Character.kt
+++ domain/src/main/java/nl/stephanmantel/domain/Character.kt
@@ -1,12 +1,16 @@
 package nl.stephanmantel.domain
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.IgnoredOnParcel
+import kotlinx.android.parcel.Parcelize
 
 internal const val TABLE_NAME_CHARACTERS = "Characters"
 
+@Parcelize
 @Entity(tableName = TABLE_NAME_CHARACTERS)
 data class Character (
     @PrimaryKey
@@ -15,7 +19,8 @@ data class Character (
 
     @ColumnInfo(name = "birthYear")
     val birthYear: Float?
-) {
+): Parcelable {
     @Ignore
+    @IgnoredOnParcel
     var isFavourite: Boolean = false
 }
