7b7a430cf7597af91b300d07ac3bc663a0a1f855
Stephan Mantel <stephan.mantel@gmail.com>
2019-01-03 10:07:59 +0100

    Add Birth year property to raw character

--- network/src/main/java/nl/stephanmantel/network/rawdomain/character/CharacterRaw.kt
+++ network/src/main/java/nl/stephanmantel/network/rawdomain/character/CharacterRaw.kt
@@ -3,5 +3,6 @@ package nl.stephanmantel.network.rawdomain.character
 import com.google.gson.annotations.SerializedName
 
 data class CharacterRaw (
-    @SerializedName("name") val name: String?
+    @SerializedName("name") val name: String?,
+    @SerializedName("birth_year") val birthYear: String?
 )
