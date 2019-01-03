7d434283866c45b02883d6f4ef0b726bc79750eb
Stephan Mantel <stephan.mantel@gmail.com>
2019-01-03 13:53:04 +0100

    Add character detil fragment

---
+++ app/src/main/java/nl/stephanmantel/starwars/characterdetail/CharacterDetailFragment.kt
@@ -0,0 +1,32 @@
+package nl.stephanmantel.starwars.characterdetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import nl.stephanmantel.starwars.R
+
+class CharacterDetailFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_character_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        configureViews()
+        observeViewModelData()
+    }
+
+    private fun configureViews() {
+
+    }
+
+    private fun observeViewModelData() {
+        
+    }
+}
