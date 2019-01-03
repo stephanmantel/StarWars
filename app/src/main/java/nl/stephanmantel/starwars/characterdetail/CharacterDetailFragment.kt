de468ad2b200213c4514f60cbbf9d2f936b5905f
Stephan Mantel <stephan.mantel@gmail.com>
2019-01-03 14:19:57 +0100

    Observe data for character details

--- app/src/main/java/nl/stephanmantel/starwars/characterdetail/CharacterDetailFragment.kt
+++ app/src/main/java/nl/stephanmantel/starwars/characterdetail/CharacterDetailFragment.kt
@@ -5,12 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.fragment_character_detail.*
+import nl.stephanmantel.domain.Character
 import nl.stephanmantel.starwars.R
+import nl.stephanmantel.starwars.common.Resource
+import nl.stephanmantel.starwars.common.Status
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 class CharacterDetailFragment: Fragment() {
 
     companion object {
         const val BUNDLE_KEY_CHARACTER = "CHARACTER"
+    }
+
+    private val viewModel: CharacterDetailViewModel by viewModel {
+        parametersOf(arguments?.getParcelable<Character>(BUNDLE_KEY_CHARACTER)!!)
     }
 
     override fun onCreateView(
@@ -31,6 +42,25 @@ class CharacterDetailFragment: Fragment() {
     }
 
     private fun observeViewModelData() {
+        viewModel.characterLiveData.observe(viewLifecycleOwner, Observer { handleCharacterChanged(it) })
+    }
 
+    private fun handleCharacterChanged(resource: Resource<Character>?) {
+        when (resource?.status) {
+            Status.LOADING -> {
+                loadingIndicator.visibility = View.VISIBLE
+                errorTextView.visibility = View.GONE
+            }
+            Status.ERROR -> {
+                loadingIndicator.visibility = View.GONE
+                errorTextView.visibility = View.VISIBLE
+                errorTextView.text = resource.error?.message
+            }
+            Status.SUCCESS -> {
+                loadingIndicator.visibility = View.GONE
+                errorTextView.visibility = View.GONE
+                characterNameTextView.text = resource.data?.name
+            }
+        }
     }
 }
