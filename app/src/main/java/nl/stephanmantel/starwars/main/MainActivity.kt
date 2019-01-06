e264234899fbf5f3214547fd2eebf435e8b35a2f
Stephan Mantel <stephan.mantel@gmail.com>
2019-01-06 19:32:26 +0100

    Setup navigation with action bar

--- app/src/main/java/nl/stephanmantel/starwars/main/MainActivity.kt
+++ app/src/main/java/nl/stephanmantel/starwars/main/MainActivity.kt
@@ -2,12 +2,24 @@ package nl.stephanmantel.starwars.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import nl.stephanmantel.starwars.R
 
 class MainActivity: AppCompatActivity() {
 
+    private val navController by lazy {
+        findNavController(R.id.navHost)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return true
     }
 }
